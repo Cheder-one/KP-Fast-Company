0e1eb0028066514fc44f078c77ec3c9dab9d859f
Cheder-one <cherbadenis1@gmail.com>
2023-02-27 18:19:59 +0300

    u

--- src/component/pagination.jsx
+++ src/component/pagination.jsx
@@ -4,16 +4,18 @@ import _ from "lodash";
 const Pagination = (props) => {
   const { itemsCount, pageSize, onPageChange } = props;
   const pageCount = Math.ceil(itemsCount / pageSize);
+
+  if (pageCount === 1) return null;
   const pages = _.range(1, pageCount + 1);
 
   return (
     <nav>
       <ul className="pagination">
-        <li className="page-item">
-          <a href="#" className="page-link">
-            1
-          </a>
-        </li>
+        {pages.map((page) => (
+          <li className="page-item" key={"page_" + page}>
+            <a className="page-link">{page}</a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
