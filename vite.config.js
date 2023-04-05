6c56aca187292f17cd52e4c83574b7803e764250
Cheder-one <cherbadenis1@gmail.com>
2023-04-06 00:03:17 +0300

    u

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslintPlugin from "vite-plugin-eslint";
-import errorOverlay from "vite-plugin-error-overlay";
 
 export default defineConfig({
-  plugins: [react(), errorOverlay(), eslintPlugin({ cache: false, fix: true })]
+  plugins: [react(), eslintPlugin({ cache: false, fix: true })]
 });
