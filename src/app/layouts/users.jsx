0d0c3d18a55cb30277decc44c58a6974c2190299
Cheder-one <cherbadenis1@gmail.com>
2023-04-23 01:16:02 +0300

    u

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UsersList from "../components/containers/usersList";
 import { useParams, Switch, Route } from "react-router-dom";
 import User from "./user";
 // import query from "query-string";
+import PropTypes from "prop-types";
+import API from "../api/index.api";
 
-const Users = () => {
+const Users = ({ allUsers }) => {
   const { userId } = useParams();
   console.log(userId);
+
+  const [users, setUsers] = useState();
+  console.log(users);
+
+  useEffect(() => {
+    API.users.fetchAll().then((users) => {
+      setUsers(users);
+      <Users allUsers={users} />;
+    });
+  }, []);
 
   return (
     <>
@@ -25,6 +37,10 @@ const Users = () => {
 };
 
 export default Users;
+
+Users.propTypes = {
+  allUsers: PropTypes.array
+};
 
 // При клике на пользователя, открывается карточка пользователя. С путем users/:userId
 // Если путь просто users, то выводим всех пользователей
