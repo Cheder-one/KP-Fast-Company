7c800fa70a16cec3804f472e46f648552cce10e7
Cheder-one <cherbadenis1@gmail.com>
2023-04-25 00:28:19 +0300

    u

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,6 +1,48 @@
 import React from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <Container className="mt-5">
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <h1 className="text-center">Login</h1>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                style={{ width: "100%" }}
+              />
+              <Form.Text className="text-muted">
+                We will never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                style={{ width: "100%" }}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Remember me" />
+            </Form.Group>
+
+            <div className="text-center">
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Login;
--- src/app/layouts/main.jsx
+++ src/app/layouts/main.jsx
@@ -1,6 +1,25 @@
 import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+
 const Main = () => {
-  return <h1>Main Page</h1>;
+  return (
+    <Container className="mt-5">
+      <Row>
+        <Col>
+          <h1 className="text-center">Welcome to the Main Page</h1>
+          <p className="text-center">
+            This is the main page of our app. Here you can find all the latest
+            news and updates.
+          </p>
+          <div className="text-center">
+            <Button href="/about" variant="primary">
+              About Us
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Main;
