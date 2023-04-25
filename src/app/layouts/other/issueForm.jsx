04443ee803f7b02d1c9e1c80653aa9272f56c7d5
Cheder-one <cherbadenis1@gmail.com>
2023-04-25 19:50:18 +0300

    u

--- src/app/layouts/other/issueForm.jsx
+++ src/app/layouts/other/issueForm.jsx
@@ -1,38 +1,45 @@
-import React from "react";
-import { Button, Form } from "react-bootstrap";
+import React, { useState } from "react";
 
 const IssueForm = () => {
+  const [email, setEmail] = useState("");
+
   const handleSubmit = () => {
     // do something...
   };
 
   const handleEmailChange = (e) => {
     const { value } = e.target;
-    console.log(value);
+    setEmail(value);
   };
 
   return (
     <div>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-2">
-          <Form.Label htmlFor="email">Email:</Form.Label>
-          <Form.Control
+      <h2>Отчёт об ошибке</h2>
+
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label htmlFor="email">Email:</label>{" "}
+          <input
             id="email"
-            type="text"
             name="email"
+            value={email}
             onChange={handleEmailChange}
           />
-        </Form.Group>
-        <Form.Group className="mb-2">
-          <Form.Label htmlFor="link">Ссылка на страницу с ошибкой:</Form.Label>
-          <Form.Control id="link" type="text" name="link" />
-        </Form.Group>
-        <Form.Group className="mb-2">
-          <Form.Label htmlFor="description">Описание:</Form.Label>
-          <Form.Control id="description" type="text" name="description" />
-        </Form.Group>
-        <Button type="submit">Отправить</Button>
-      </Form>
+        </p>
+        <p>
+          <label htmlFor="link">Ссылка на страницу с ошибкой:</label>{" "}
+          <input id="link" name="link" />
+        </p>
+
+        <p>
+          <label htmlFor="description">Описание:</label>{" "}
+          <input id="description" name="description" />
+        </p>
+
+        <button className="btn btn-outline-primary" type="submit">
+          Отправить
+        </button>
+      </form>
     </div>
   );
 };
