1d4d29bc4486f066fd707891b7ae8804c8ca48e1
Cheder-one <cherbadenis1@gmail.com>
2023-04-25 20:40:59 +0300

    u

--- src/app/layouts/other/issueForm.jsx
+++ src/app/layouts/other/issueForm.jsx
@@ -1,41 +1,63 @@
 import React, { useState } from "react";
 
 const IssueForm = () => {
-  const [email, setEmail] = useState("");
+  const [values, setValues] = useState({
+    email: "",
+    link: "",
+    desc: ""
+  });
 
   const handleSubmit = () => {
     // do something...
   };
 
-  const handleEmailChange = (e) => {
-    const { value } = e.target;
-    setEmail(value);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prev) => ({
+      ...prev,
+      [name]: value
+    }));
   };
+
+  const { email, link, desc } = values;
 
   return (
     <div>
       <h2>Отчёт об ошибке</h2>
 
       <form onSubmit={handleSubmit}>
-        <p>
+        <div className="form-group">
           <label htmlFor="email">Email:</label>{" "}
           <input
+            className="form-control"
             id="email"
             name="email"
             value={email}
-            onChange={handleEmailChange}
+            onChange={handleInputChange}
           />
-        </p>
-        <p>
+        </div>
+        <div className="form-group">
           <label htmlFor="link">Ссылка на страницу с ошибкой:</label>{" "}
-          <input id="link" name="link" />
-        </p>
+          <input
+            className="form-control"
+            id="link"
+            name="link"
+            value={link}
+            onChange={handleInputChange}
+          />
+        </div>
 
-        <p>
+        <div className="form-group">
           <label htmlFor="description">Описание:</label>{" "}
-          <input id="description" name="description" />
-        </p>
-
+          <input
+            className="form-control"
+            id="description"
+            name="desc"
+            value={desc}
+            onChange={handleInputChange}
+          />
+        </div>
+        <br />
         <button className="btn btn-outline-primary" type="submit">
           Отправить
         </button>
