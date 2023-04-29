a16bbd855a731c43d4f4d6c3561b1e8b5f0f50dc
Cheder-one <cherbadenis1@gmail.com>
2023-04-29 17:40:54 +0300

    u

--- src/app/utils/validators/validationSchema.js
+++ src/app/utils/validators/validationSchema.js
@@ -10,11 +10,14 @@ export const loginSchema = {
   password: {
     isRequired: {
       message: "Поле Password обязательно"
+    },
+    minLength: {
+      allowValue: 8,
+      message: "Минимальная длина пароля 8 символов"
+    },
+    isStrong: {
+      message: "Пароль должен состоять из заглавных, строчных букв и цифр"
     }
-    // minLength: {
-    //   allowValue: 6,
-    //   message: "Минимальная длина пароля 6 символов"
-    // }
   }
 };
 
@@ -22,10 +25,10 @@ export const issueFormSchema = {
   email: {
     isRequired: {
       message: "Поле Email обязательно"
+    },
+    isEmail: {
+      message: "Введите корректный Email"
     }
-    // isEmail: {
-    //   message: "Введите корректный Email"
-    // }
   },
   link: {
     isRequired: {
