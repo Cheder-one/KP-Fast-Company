38ae3bbccf9481831913a5ba5e13ed3ef04fbe8b
Cheder-one <cherbadenis1@gmail.com>
2023-05-01 00:56:58 +0300

    u

--- src/app/utils/templates/textField.jsx
+++ src/app/utils/templates/textField.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { eyeFill, eyeSlash } from "../../assets/show-hide-pass-svg";
 
 const TextField = ({ label, type, id, name, value, onChange, error }) => {
+  const [showPass, setShowPass] = useState(false);
+
   const getInputClasses = () => {
     return "form-control " + (error ? "is-invalid" : "is-valid");
   };
+
+  const toggleShowPass = () => {
+    setShowPass((perv) => !perv);
+  };
+
   return (
     <div className="mb-4">
       <label htmlFor={id}>{label}</label>
-      <input
-        className={getInputClasses()}
-        type={type}
-        id={id}
-        name={name}
-        value={value}
-        onChange={onChange}
-      />
-      <div className="invalid-feedback">{error}</div>
+      <div className="input-group has-validation">
+        <input
+          className={getInputClasses()}
+          type={showPass ? "text" : type}
+          id={id}
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
+        {type === "password" && (
+          <button
+            type="button"
+            className="btn btn-outline-primary"
+            onClick={toggleShowPass}
+          >
+            {showPass ? eyeSlash : eyeFill}
+          </button>
+        )}
+        <div className="invalid-feedback">{error}</div>
+      </div>
     </div>
   );
 };
