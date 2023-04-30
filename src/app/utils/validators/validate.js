0a0dee71e29b89a1b23aec2875614811f1f7f335
Cheder-one <cherbadenis1@gmail.com>
2023-04-30 21:19:48 +0300

    u

--- src/app/utils/validators/validate.js
+++ src/app/utils/validators/validate.js
@@ -1,12 +1,4 @@
-import {
-  isEmail,
-  isStrongPass,
-  isRequired,
-  minLength,
-  maxLength,
-  isUrl
-} from "./validateRules";
-
+import validateRules from "./validateRules";
 export const validate = (inputFields, config) => {
   const errors = {};
 
@@ -16,7 +8,9 @@ export const validate = (inputFields, config) => {
 
     for (const ruleName in rulesForField) {
       const { message, allowValue } = rulesForField[ruleName];
-      const hasError = !validator(ruleName, fieldVal, allowValue);
+
+      const validator = validateRules[ruleName];
+      const hasError = validator && !validator(fieldVal, allowValue);
 
       if (hasError) {
         errors[field] = message;
@@ -26,22 +20,3 @@ export const validate = (inputFields, config) => {
   }
   return errors;
 };
-
-function validator(ruleName, value, allowValue) {
-  switch (ruleName) {
-    case "isRequired":
-      return isRequired(value);
-    case "isEmail":
-      return isEmail(value);
-    case "isUrl":
-      return isUrl(value);
-    case "isStrongPass":
-      return isStrongPass(value);
-    case "minLength":
-      return minLength(value, allowValue);
-    case "maxLength":
-      return maxLength(value, allowValue);
-    default:
-      return true;
-  }
-}
--- src/app/utils/validators/validateRules.js
+++ src/app/utils/validators/validateRules.js
@@ -1,3 +1,13 @@
+export default {
+  isRequired: (value) => Boolean(value.trim()),
+  isEmail: (value) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value),
+  isUrl: (value) => /^https?:\/\/(www.)?[^\s]+$/g.test(value),
+  isStrongPass: (value) => /(?=.*[A-ZА-Я])(?=.*[a-zа-я])(?=.*\d)/g.test(value),
+  minLength: (value, allowValue) => value.length > allowValue,
+  maxLength: (value, allowValue) => value.length < allowValue
+};
+
+/*
 export const isRequired = (value) => Boolean(value.trim());
 
 export const isEmail = (value) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
@@ -5,9 +15,7 @@ export const isEmail = (value) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
 export const isUrl = (value) => /^https?:\/\/\S+$/.test(value);
 
 export const isStrongPass = (value) =>
-  /(?=.*[A-ZА-Я])(?=.*[a-zа-я])(?=.*\d).+/g.test(value);
-isStrongPass("1Вв");
-isStrongPass("1fff @ffffП   ");
+  /(?=.*[A-ZА-Я])(?=.*[a-zа-я])(?=.*\d)/g.test(value);
 
 export const minLength = (value, allowValue) => {
   // const { allowValue } = config.password.minLength;
@@ -18,12 +26,4 @@ export const maxLength = (value, allowValue) => {
   // const { allowValue } = config.description.maxLength;
   return value.length < allowValue;
 };
-
-export default {
-  isRequired: (value) => Boolean(value.trim()),
-  isEmail: (value) => /^https?:\/\/(www.)?[^\s]+$/g.test(value),
-
-  min: (value, length) => value.length >= length,
-  isCapitalSymbol: (value) => /[A-Z]+/g.test(value),
-  isContainDigit: (value) => /d+/g.test(value)
-};
+*/
