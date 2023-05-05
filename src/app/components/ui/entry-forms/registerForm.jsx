9411c98ceeead8eeb18376595f16639633079344
Cheder-one <cherbadenis1@gmail.com>
2023-05-05 16:26:02 +0300

    u

--- src/app/components/ui/entry-forms/registerForm.jsx
+++ src/app/components/ui/entry-forms/registerForm.jsx
@@ -3,10 +3,24 @@ import PropTypes from "prop-types";
 import TextField from "../../common/form/textField";
 import { validate } from "../../../utils/validators/validate";
 import { loginSchema } from "../../../utils/validators/validationSchema";
+import API from "../../../api/index.api";
 
 const RegisterForm = ({ text }) => {
-  const [inputFields, setInputFields] = useState({ email: "", password: "" });
+  const [inputFields, setInputFields] = useState({
+    email: "",
+    password: "",
+    profession: ""
+  });
+  const { email, password, profession: selectedProf } = inputFields;
+
   const [errors, setErrors] = useState({});
+
+  const [professions, setProfessions] = useState();
+  useEffect(() => {
+    API.professions.fetchAll().then((profs) => {
+      setProfessions(profs);
+    });
+  }, []);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -29,8 +43,6 @@ const RegisterForm = ({ text }) => {
     console.log(inputFields);
   };
 
-  const { email, password } = inputFields;
-
   return (
     <form onSubmit={handleSubmit}>
       <TextField
@@ -51,7 +63,32 @@ const RegisterForm = ({ text }) => {
         onChange={handleInputChange}
         error={errors.password}
       />
-
+      <div className="mb-4">
+        <label htmlFor="profFormSelect" className="form-label">
+          Ваша профессия:
+        </label>
+        <select
+          className="form-select"
+          id="profFormSelect"
+          name="profession"
+          value={selectedProf}
+          onChange={handleInputChange}
+        >
+          <option disabled value="">
+            Choose...
+          </option>
+          {professions &&
+            Object.keys(professions).map((profName) => {
+              const profInfo = professions[profName];
+              return (
+                <option key={profInfo._id} value={profInfo._id}>
+                  {profInfo.name}
+                </option>
+              );
+            })}
+        </select>
+        <div className="invalid-feedback">Please select a valid state.</div>
+      </div>
       <button
         disabled={hasErrors}
         className={"btn btn-primary w-100 mx-auto"}
--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 import React, { useState } from "react";
 import LoginForm from "../components/ui/entry-forms/loginForm";
-import { Link, useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import RegisterForm from "../components/ui/entry-forms/registerForm";
 
 const Login = () => {
@@ -26,22 +26,22 @@ const Login = () => {
           {formType === "register" ? (
             <>
               <RegisterForm text={"Регистрация"} />
-              <p className="mt-1">
+              <div className="mt-1">
                 Already have an account?{" "}
                 <a role="button" onClick={toggleFormType}>
                   <span className="text-primary">Login</span>
                 </a>
-              </p>
+              </div>
             </>
           ) : (
             <>
               <LoginForm text={"Вход"} />
-              <p className="mt-1">
+              <div className="mt-1">
                 Don't have an account?{" "}
                 <a role="button" onClick={toggleFormType}>
                   <span className="text-primary">SignUp</span>
                 </a>
-              </p>
+              </div>
             </>
           )}
         </div>
