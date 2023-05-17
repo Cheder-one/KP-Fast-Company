fab2869cd69301e10e10b5180489452f151923ab
Cheder-one <cherbadenis1@gmail.com>
2023-05-17 21:49:26 +0300

    filling UserEditForm Component. adding yup schema

--- src/app/components/ui/user-edit-form/userEditForm.jsx
+++ src/app/components/ui/user-edit-form/userEditForm.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "../../../layouts/form";
+import TextField from "../../common/form/textField";
 
 const UserEditForm = () => {
-  return <h1>UserEditForm</h1>;
+  const [inputFields, setInputFields] = useState({
+    fio: "",
+    email: "",
+    profession: "",
+    gender: "",
+    qualities: []
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInputFields((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+  return (
+    <Form>
+      <TextField
+        label="Имя"
+        name="fio"
+        value={inputFields.fio}
+        onChange={handleInputChange}
+        error=""
+      />
+      <TextField
+        label="Email"
+        name="email"
+        value={inputFields.email}
+        onChange={handleInputChange}
+        error=""
+      />
+    </Form>
+  );
 };
 
 export default UserEditForm;
